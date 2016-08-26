560a35243291de19633242832d7f1ece2f34d2a8
Batz74 <kick-boxcing@mail.ru>
2016-08-26 21:47:08 +0500

    First class

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class TestClass
+    {
+        public string TestProp { get; set; }
+
+        public TestClass()
+        {
+
+        }
+
+        public void DoSOmething()
+        {
+
+        }
+    }
+}
